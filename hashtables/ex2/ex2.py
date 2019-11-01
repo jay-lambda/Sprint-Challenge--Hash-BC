aaa730125f720596fb7ca9c924aba1f08446491a
JaySebagh <jaysebagh@gmail.com>
2019-11-01 09:14:26 -0700

    ex2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -16,8 +16,28 @@ def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
     route = [None] * length
 
-    """
-    YOUR CODE HERE
-    """
-
+    # insert all the tickets into the hastable with the source as key and destination as value
+    for i in tickets:
+        hash_table_insert(hashtable, i.source, i.destination)
+    
+    # ticket destination will retrieve the destination value based off the source key
+    ticket_destination = hash_table_retrieve(hashtable, "NONE")
+    
+    # loop over every flight
+    for i in range(0, length):
+        # add the destination to the route
+        route[i] = ticket_destination
+        # replace the source key in ticket destination with the destination value
+        ticket_destination = hash_table_retrieve(hashtable, ticket_destination)
+    
+    # print(route)
     return route
+
+# ticket_1 = Ticket("NONE", "PDX")
+# ticket_2 = Ticket("PDX", "DCA")
+# ticket_3 = Ticket("DCA", "NONE")
+
+# tickets = [ticket_1, ticket_2, ticket_3]
+
+# # expected = ["PDX", "DCA", "NONE"]
+# result = reconstruct_trip(tickets, 3)
